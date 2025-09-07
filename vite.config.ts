c4b885646b0cffcc15d996e099956267f36a8aff
Felix Calel <felixcalel806@gmail.com>
2025-09-07 14:41:33 -0600

    first steps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
+import dotenv from 'dotenv'
 
-// https://vite.dev/config/
+dotenv.config()
+
+if (!process.env.VITE_PORT) {
+  throw new Error("La variable de entorno VITE_PORT no está definida en el archivo .env")
+}
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: true,
+    port: parseInt(process.env.VITE_PORT),
+    cors: true,
+  },
 })
