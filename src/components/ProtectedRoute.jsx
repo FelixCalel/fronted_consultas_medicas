041f903b4a8a0b8c10596ce74442ddca950e3e3c
Ale20GT <145415938+Ale20GT@users.noreply.github.com>
2025-10-21 23:49:25 -0600

    Cambioseliminados

--- src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/ProtectedRoute.jsx
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-
-export default function ProtectedRoute() {
-  const token = localStorage.getItem('token')
-  if (!token) return <Navigate to="/login" replace />
-  return <Outlet />
-}
